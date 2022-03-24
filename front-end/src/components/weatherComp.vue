f3a4b0b7bc28d78b398bcec5357a58998f78801b
geeraaldiinee <95378325+geeraaldiinee@users.noreply.github.com>
2022-03-24 07:49:03 -0400

    Update by Gee
    added weather forecast initial view

---
+++ front-end/src/components/weatherComp.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="sensorCon">
+            <!-- <div id="light" class="sensor">
+                <img src="/static/sunicon.svg" alt="lightIcon">
+                <p class="subHeading">Light</p>
+                <div class="infoCon" id="lightSensor">
+                    <canvas ref="lightChart"></canvas>
+                    <p></p>
+                    <p></p>
+                </div>
+            </div>
+            <div id="moisture" class="sensor">
+                <img src="/static/moistureicon.svg" alt="mositure icon">
+                <p class="subHeading">Moisture</p>
+                <div class="infoCon" id="moistureSensor">
+                    <canvas ref="moistChart"></canvas>
+                    <p></p>
+                    <p></p>
+                </div>
+            </div>
+            <div id="temprature" class="sensor">
+                <img src="/static/tempicon.svg" alt="temprature icon">
+                <p class="subHeading">Temprature</p>
+                <div class="infoCon" id="tempSensor">
+                    <canvas ref="tempChart" class="rotated"></canvas>
+                    <p></p>
+                    <p></p>
+                </div>
+            </div> -->
+        </div>
+
+</template>
+
+<script>
+export default {
+    created() {
+    },
+
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .rotated {
+        transform: rotate(270deg);
+    }
+</style>
---
+++ front-end/src/views/weather-view.vue
@@ -0,0 +1,57 @@
+<template>
+    <main>
+        <nav-menu></nav-menu>
+        <p class="heading">Your daily weather forecast</p>
+        
+        <weather-comp></weather-comp>
+    </main>
+</template>
+
+<script>
+import NavMenu from "../components/navmenu";
+import WeatherComp from '../components/weatherComp'
+export default {
+    components: {NavMenu, WeatherComp}
+}
+</script>
+
+<style lang="scss">
+    .sensorCon{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        flex-direction: column;
+    }
+
+    @media screen and (min-width: 768px){
+        .heading{
+            font-size: 32px;
+        }
+
+        .sensor{
+            margin-bottom: 10px;
+        }
+
+    @media screen  and (min-width: 1024px){
+        .heading{
+            margin-top: 20px;
+            margin-left: 100px;
+            margin-right: 100px;
+            font-weight: 400;
+            font-size: 24px;
+        }
+
+        .sensorCon{
+            display: grid; 
+            grid-template-columns: 1fr 1fr 1fr; 
+            gap: 30px 30px;
+            margin-left: 100px;
+            margin-right: 100px; 
+        }
+
+
+    }
+
+    }
+</style>
